fc9e9513e90f3aa632ace06b8601499cc0859ce8
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:43:14 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PEDRO CAPÓ';
+export let nameSongs = [
+	"CALMA", /*1*/
+	"TUTU (REMIX)", /*2*/
+	"LA SÁBANA Y LOS PIES", /*3*/
+	"TE OLVIDARÉ", /*4*/
+	"FIESTA", /*5*/
+	"UN MINUTO", /*6*/
+	"AZÚCAR AMARGO", /*7*/
+	"VAMOS A HUÍR", /*8*/
+	"TU FANÁTICO", /*9*/
+	"5 Y 3", /*10*/
+	"LA NETA", /*11*/
+	"PARA AYUDARTE A REIR", /*12*/
+	"FIEBRE DE AMOR", /*13*/
+	"DUELE SER INFIEL", /*14*/
+	"VALIO LA PENA", /*15*/
+	"VAMOS A HUIR", /*16*/
+	"WOULD YOU LET ME GO", /*17*/
+	"OJOS CLAROS", /*18*/
+	"NI TAN NOVIOS, NI TAN AMIGOS", /*19*/
+	"UNA VEZ MÁS", /*20*/
+]
